a90cd1519ced6aaaef164ca2e07eaa7ffa460621
Keno Schwalb <keno@kenoschwalb.com>
2016-07-29 11:07:07 +0200

    test(throttled-transform): add test cases

---
+++ test/lib/helpers.js
@@ -0,0 +1,25 @@
+import ThrottledTransform from '../../src/throttled-transform';
+
+/**
+ * Returns a mock ThrottledTransform stream
+ * @param {Function} throttledTransform The transformation function
+ * @param {number}   qps The maximum number of queries per second
+ * @return {Stream} A ThrottledTransform stream
+ **/
+export function getThrottledTransformStream(
+  throttledTransform = (data, done) => {
+    done(null, data);
+  },
+  qps = 35 
+) {
+  class TransformTestClass extends ThrottledTransform {
+    constructor() {
+      super(qps);
+    }
+  }
+
+  TransformTestClass.prototype // eslint-disable-line no-underscore-dangle
+    ._throttledTransform = throttledTransform;
+
+  return TransformTestClass;
+}
---
+++ test/throttled-transform.js
@@ -0,0 +1,80 @@
+/* eslint-disable no-unused-expressions */
+import test from 'ava';
+import sinon from 'sinon';
+import ThrottledTransform from '../src/throttled-transform';
+import {getThrottledTransformStream} from './lib/helpers';
+
+test.cb('should call a child\'s _throttledTransform function on write', t => { // eslint-disable-line max-len
+  const transformStub = sinon.stub().callsArgWith(2),
+    ThrottledTransformStub = getThrottledTransformStream(transformStub),
+    transformInstance = new ThrottledTransformStub(),
+    data = 'some data';
+
+  transformInstance.on('data', () => {});
+  transformInstance.on('end', () => {
+    t.true(transformStub.calledOnce);
+    t.is(transformStub.args[0][0].toString(), data);
+    t.end();
+  });
+
+  transformInstance.write(data);
+  transformInstance.end();
+});
+
+test.cb('should emit the _throttledTransform function\'s data', t => {
+  const result = 'some result',
+    ThrottledTransformStub = getThrottledTransformStream(
+      sinon.stub().callsArgWith(2, null, result)
+    ),
+    transformInstance = new ThrottledTransformStub();
+
+  transformInstance.on('data', data => {
+    t.is(data.toString(), result);
+  });
+  transformInstance.on('end', t.end);
+
+  transformInstance.write('some data');
+  transformInstance.end();
+});
+
+test('should throw an error when not implementing _throttledTransform', t => {
+  class BrokenThrottledTransform extends ThrottledTransform {}
+  const transformInstance = new BrokenThrottledTransform();
+
+  t.throws(() => {
+    transformInstance.write('some data');
+  }, 'Not implemented');
+});
+
+test.cb('should pass options on to the stream.Transform constructor', t => {
+  const transformStub = sinon.stub().callsArg(2),
+    data = {someKey: 'someValue'},
+    maxThrottled = 1,
+    options = {objectMode: true},
+    ThrottledTransformStub = getThrottledTransformStream(
+      transformStub,
+      maxThrottled,
+      options
+    ),
+    transformInstance = new ThrottledTransformStub();
+
+  transformInstance.on('data', result => {
+    t.deepEqual(result, data);
+  });
+
+  transformInstance.on('end', () => {
+    t.true(transformStub.calledOnce);
+    t.end();
+  });
+
+  transformInstance.write(data);
+  transformInstance.end();
+});
+
+test('should allow instantiation via function call', t => {
+  const stream = ThrottledTransform.create(1, (data, encoding, done) => {
+    done(null, data);
+  });
+
+  t.true(stream instanceof ThrottledTransform);
+});
